04c6ebaeb2a975218a9875c457b48a24e98177ef
Stanislav Streltsov <strelcov.stanislav93@gmail.com>
2022-01-07 00:05:11 +0200

    chore: add eslint and prettier libs for code formatting

---
+++ .eslintrc.js
@@ -0,0 +1,46 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+  },
+  extends: ['airbnb-base', 'prettier', 'plugin:node/recommended'],
+  parserOptions: {
+    ecmaVersion: 13,
+  },
+  plugins: ['prettier'],
+  rules: {
+    'prettier/prettier': ['error', { endOfLine: 'auto' }],
+    'no-unused-vars': 'warn',
+    'no-console': 'off',
+    'func-names': 'off',
+    'no-process-exit': 'off',
+    'object-shorthand': 'off',
+    'class-methods-use-this': 'off',
+    'no-underscore-dangle': 'off',
+    'no-shadow': ['error', { allow: ['done', 'err', 'res', 'doc'] }],
+    'node/no-unsupported-features/es-syntax': [
+      'error',
+      {
+        version: '>=8.3.0',
+        ignores: ['spreadElements'],
+      },
+    ],
+    camelcase: [
+      'error',
+      {
+        properties: 'never',
+        ignoreDestructuring: true,
+        allow: [
+          'issue_title',
+          'issue_text',
+          'created_on',
+          'updated_on',
+          'created_by',
+          'assigned_to',
+          'status_text',
+        ],
+      },
+    ],
+  },
+};
--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -1,9 +1,17 @@
-const chai = require('chai');
+const chai = require("chai");
 const assert = chai.assert;
 
-const Solver = require('../controllers/sudoku-solver.js');
-let solver;
+const Solver = require("../controllers/sudoku-solver.js");
+let solver = new Solver();
 
-suite('UnitTests', () => {
-
+suite("UnitTests", () => {
+  // Logic handles a valid puzzle string of 81 characters
+  // Logic handles a puzzle string with invalid characters (not 1-9 or .)
+  // Logic handles a puzzle string that is not 81 characters in length
+  assert.isTrue(
+    solver.validate(
+      "1.5..2.84..63.12.7.2..5.....9..1....8.2.3674.3.7.2..9.47...8..1..16....926914.37.",
+      "puzzle string should be valid"
+    )
+  );
 });
